af4b28d005fe465ede126e4e539a14f4cbab1aea
Daniel Luque <danielluque@Daniels-MacBook-Air.local>
2024-02-17 18:28:44 -0500

    Started the responsive stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server',
   site: 'https://capitaleme.com',
   image: {
     domains: ["astro.build"]
