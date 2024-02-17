8ccc1a6040faed279809aa176d7c264c9dc07824
Daniel Luque <danielluque@Daniels-MacBook-Air.local>
2024-02-17 15:10:07 -0500

    Changed the api google script to react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
 import astroExpressiveCode from 'astro-expressive-code';
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
