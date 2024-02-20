45a45060d2992889df1b08f7aecf27283380959b
Daniel Luque <danielluque@Daniels-MacBook-Air.local>
2024-02-20 05:51:23 -0500

    Add copy to section 2 of the home page

---
+++ src/components/es/home/newFile.tsx
@@ -0,0 +1,38 @@
+<Fragment>
+<section class="bg-white h-screen flex">
+{/** Left column */}
+{/** section title  */}
+Visas & Immigration
+
+{/** subsection title */}
+Want to stay in Colombia just a bit  longer?
+
+{/** subsection text */}
+With our Visas Assistance Services, you will have an expert bilingual immigration attorney by your side to help you plan your move to Colombia.
+
+{/** Carousel of visas */}
+
+{/** End Carousel of visas */}
+
+{/** section title */}
+Other services
+
+{/** subsection title */}
+Want to buy property in Colombia?
+{/** btn */}
+Contact now
+
+{/** End of Left Column */}
+
+
+{/** Right Column */}
+{/** subsection title */}
+Need Assistance with the Migration authority?
+
+
+
+
+{/** End of Right Column */}
+
+</section>
+</Fragment>;
