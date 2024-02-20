758c33521c1844c84c540c4464fca6d63f5e4e60
Daniel Luque <danielluque@Daniels-MacBook-Air.local>
2024-02-20 13:24:04 -0500

    Add CORS script

--- src/pages/api/google/reviews.ts
+++ src/pages/api/google/reviews.ts
@@ -33,6 +33,7 @@ export const GET: APIRoute = async () => {
         status: 200,
         headers: {
           'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
         },
       });
     } else {
