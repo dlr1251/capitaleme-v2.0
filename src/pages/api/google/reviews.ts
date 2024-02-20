503640d75e733a6109b708004a40f2fa36e86f71
Daniel Luque <danielluque@Daniels-MacBook-Air.local>
2024-02-20 13:29:47 -0500

    Add specific domain on CORS for the reviews bug

--- src/pages/api/google/reviews.ts
+++ src/pages/api/google/reviews.ts
@@ -33,7 +33,7 @@ export const GET: APIRoute = async () => {
         status: 200,
         headers: {
           'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Origin': 'https://www.capitaleme.com/',
         },
       });
     } else {
