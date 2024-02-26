e47dcd4ef3568446a11353740f4d512f15999f71
Daniel Luque <danielluque@Daniels-MacBook-Air.local>
2024-02-26 10:34:03 -0500

    Add Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import astroExpressiveCode from 'astro-expressive-code';
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
@@ -25,11 +27,21 @@ export default defineConfig({
       prefixDefaultLocale: false
     }
   },
-  integrations: [astroExpressiveCode(), mdx(), tailwind({
-    applyBaseStyles: true,
-    configFile: './tailwind.config.mjs'
-  }), react()],
+  integrations: [
+    astroExpressiveCode(), 
+    mdx(), 
+    tailwind({
+      applyBaseStyles: true,
+      configFile: './tailwind.config.mjs'
+    }), 
+    react(), 
+    partytown({      
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    })
+  ],
   adapter: vercel({
-    edgeMiddleware: true,
+    edgeMiddleware: true
   })
 });
