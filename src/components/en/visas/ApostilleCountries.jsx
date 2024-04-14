a8eb214ea9842154e670e968a8720ed373dc4db8
Daniel Luque <danielluque@Daniels-MacBook-Air.local>
2024-04-13 20:21:12 -0500

    Refactor a little

--- src/components/en/visas/ApostilleCountries.jsx
+++ src/components/en/visas/ApostilleCountries.jsx
@@ -32,7 +32,7 @@ const SearchComponent = () => {
       <button 
             type="button"
             onClick={() => setToggle(!toggle)} 
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2">
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2">
           Countries that are part of the Apostille Convention
           { toggle ? 
           <svg class="w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
