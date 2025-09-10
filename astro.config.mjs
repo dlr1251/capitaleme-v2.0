b8ad4a7828a13042eaff3e913b85281d0f39480e
Daniel Luque <danielluque@Daniel-Luque---Apple-M1.local>
2025-09-10 07:57:40 -0500

    Fix redirect from / to /en by setting prefixDefaultLocale to false

--- astro.config.mjs
+++ astro.config.mjs
@@ -72,7 +72,7 @@ export default defineConfig({
     defaultLocale: "en",
     locales: ["en", "es"],
     routing: {
-      prefixDefaultLocale: true
+      prefixDefaultLocale: false
     }
   },
   integrations: [
